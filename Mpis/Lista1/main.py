14176ca065f379cdc73e89534e9326eb47cb9fbe
Antlion <antlion@pop-os.localdomain>
2022-11-14 14:07:09 +0100

    Dodanie odliczania do Listy 1 z Mpis

--- Mpis/Lista1/main.py
+++ Mpis/Lista1/main.py
@@ -5,6 +5,7 @@ import sys
 import math
 from scipy.integrate import quad
 from scipy.optimize import minimize_scalar
+from datetime import datetime, timedelta
 
 a = float(sys.argv[1])
 b = float(sys.argv[2])
@@ -13,16 +14,26 @@ def function(x):
     return eval(sys.argv[3])
 
 M = -minimize_scalar(lambda x: -function(x), bounds=[a,b], method='bounded').fun + 1
+steps = 0
+previousTime = datetime.now()
+lastePercent = round(steps/126250)
 
-for n in range(50, 5000, 50):
+for n in range(50, 5050, 50):
     sum = 0
-    for _ in range(50):
 
+    for _ in range(50):
+        
         counter = 0
 
         for _ in range(n):
+            if(round(steps/126250) > lastePercent):
+                lastePercent = round(steps/126250)
+                timeNow = datetime.now()
+                difference = timeNow - previousTime
+                previousTime = timeNow
+                print(lastePercent,"%, estimated time: " ,(difference * (100 - lastePercent)).seconds," seconds" , end="     \r")
             x = a + (b-a) * random.random()
-
+            steps += 1
             if(M * random.random() <= function(x)):
                 counter = counter + 1
         plt.plot(n, (counter/n)*(b-a)*M, 'o', color = 'blue', markersize=2)
